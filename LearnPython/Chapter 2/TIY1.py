013961e7d80511bbcde0ec12edc7fec450a7da8d
xywgo <xywgo@hotmail.com>
2020-04-28 20:23:48 +0800

    Try It YourSelf:String

---
+++ LearnPython/Chapter 2/TIY1.py
@@ -0,0 +1,28 @@
+# 2-3 Personal Message
+fisrt_name = "jacob"
+last_name = "yang"
+massage = f"Hello {fisrt_name}, would you like to learn some Python today?"
+print(massage)
+
+# 2-4 Name Cases:
+print(fisrt_name.title())
+print(fisrt_name.upper())
+print(fisrt_name.lower())
+
+# 2-5 Famous Quote
+quote = 'Albert Einstein once said, "A person who nerver made a mistake never tried anythine new."'
+print(quote)
+
+# 2-6 Famous Quote 2
+famous_person = "a famous person"
+massage = f'{famous_person} once siad, "A person who nerver made a mistake never tried anythine new."'
+print(massage)
+
+# Stripping Names
+name = ' \tJason \n\tChen '
+print(name)
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
+
+
