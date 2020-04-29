26acbe2a0332dcc374949e7cbe6ed076ec084450
xywgo <xywgo@hotmail.com>
2020-04-29 10:59:54 +0800

    what is list

---
+++ LearnPython/Chapter 3/bicycles.py
@@ -0,0 +1,9 @@
+bicycles = ['trek', 'cannodale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
+massage = f"My first bicycle was a {bicycles[0].title()}."
+print(massage)
